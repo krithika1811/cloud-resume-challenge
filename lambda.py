21bc685dcb3f9c2095b912fa043ddb8f618d832c
krithika1811 <krithika1811@gmail.com>
2024-01-17 11:24:48 -0800

    changes in main.yml

--- lambda.py
+++ lambda.py
@@ -1,17 +1,29 @@
 import json
 import boto3
+
+# Initialize DynamoDB resource and specify the table name
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('cloudresume')
-def lambda_handler(event,context):
+table = dynamodb.Table('cloud-resume')
+
+# Lambda function handler
+def lambda_handler(event, context):
+    # Retrieve the current 'views' count from DynamoDB
     response = table.get_item(Key={
-        'id':'1'
+        'id': '0'
     })
-    views = response['Item']['views']
-    views = views + 1
-    print(views)
+
+    # Extract the 'views' count from the DynamoDB response
+    current_views = response.get('Item', {}).get('views', 0)
+
+    # Increment the 'views' count by 1
+    updated_views = current_views + 1
+    print(f"Previous views: {current_views}, Updated views: {updated_views}")
+
+    # Update the 'views' count in DynamoDB
     response = table.put_item(Item={
-        'id':'1',
-        'views':views
+        'id': '0',
+        'views': updated_views
     })
-    
-    return views
+
+    # Return the updated 'views' count
+    return updated_views
